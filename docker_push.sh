62cbc14f3b937f2065c68597a1618c92ddc61ce6
Thomas Prescott <tcprescott@gmail.com>
2018-11-12 23:41:30 -0600

    another attempt at tagged pushes

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
-docker push tcprescott/tileroombot
+export REPO=tcprescott/tileroombot
+export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+docker tag $REPO:$COMMIT $REPO:$TAG
+docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+docker push $REPO
