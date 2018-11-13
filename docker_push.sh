9df21d18d3c246ad2121eea3f9f13a6c7049c16f
Thomas Prescott <tcprescott@gmail.com>
2018-11-12 23:44:02 -0600

    leave COMMIT out as we're not going to use it

--- docker_push.sh
+++ docker_push.sh
@@ -2,6 +2,6 @@
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 export REPO=tcprescott/tileroombot
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-docker tag $REPO:$COMMIT $REPO:$TAG
-docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+docker tag $REPO $REPO:$TAG
+docker tag $REPO $REPO:travis-$TRAVIS_BUILD_NUMBER
 docker push $REPO
