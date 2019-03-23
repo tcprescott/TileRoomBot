f221fec7fd660cd86deacb6541d3e6bc923a59dd
Thomas Prescott <tcprescott@gmail.com>
2019-03-22 22:33:33 -0500

    remove all of this docker stuff, we're going to be dropping the use of docker

--- docker_push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
-export REPO=tcprescott/tileroombot
-export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-docker tag $REPO $REPO:$TAG
-docker tag $REPO $REPO:travis-$TRAVIS_BUILD_NUMBER
-docker push $REPO
